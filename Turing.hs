b0b9731ca6ad5d53ec4d1d1e6a96654dd2ae4cba
Ulf Norell <ulf.norell@gmail.com>
2025-03-23 11:46:38 +0100

    lazy exploration

--- Turing.hs
+++ Turing.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ViewPatterns #-}
 module Main where
 
@@ -8,6 +9,7 @@ import Control.Monad
 import Data.Bits
 import Data.List
 import Data.Ord
+import Data.Maybe
 import Data.Map (Map)
 import Data.Map.Strict qualified as Map
 import Data.Set (Set)
@@ -19,6 +21,7 @@ import Data.Array.Base (unsafeAt)
 import Data.Word
 import Data.Char
 import Text.Printf
+import Test.QuickCheck
 
 ------------------------------------------------------------------
 -- a Symbol is what is written on the tape
@@ -118,6 +121,19 @@ vrun n m = vizrun 200 n m 0 initConf >>= print . fst
 score :: Machine -> Int
 score rs = fst (run rs 0 initConf)
 
+-- Reachability graphs ----------------------------------------------------
+
+type ReachGraph a = Map a (Set a)
+
+addReachEdge :: Ord a => a -> a -> ReachGraph a -> ReachGraph a
+addReachEdge x y g = addYs <$> (Map.insertWith (<>) x (Set.singleton y) g)
+  where
+    ys = Set.insert y $ fromMaybe mempty (Map.lookup y g)
+    addYs zs
+      | Set.member x zs = ys <> zs
+      | Set.member y zs = ys <> zs
+      | otherwise       = zs
+
 -- Smarter running --------------------------------------------------------
 
 data Reason = GiveUp | StuckLeft | RunAway | NoPath | Loop | TooWide
@@ -125,29 +141,23 @@ data Reason = GiveUp | StuckLeft | RunAway | NoPath | Loop | TooWide
 
 data LoopAnalysis = LoopAnalysis
   { leftStuck    :: [(State, Symbol)]
-  , noPathToHalt :: [(State, Symbol)]
+  , runners      :: [State]
   }
   deriving Show
 
 loopAnalysis :: Machine -> LoopAnalysis
 loopAnalysis m = LoopAnalysis
-  { leftStuck    = goLeftStuck [ ((s1, i), (s2, o)) | (s1, i) :-> (s2, o, L) <- m, s2 /= H ]
-  , noPathToHalt = goNoPath m
+  { leftStuck = go [ ((s1, i), (s2, o)) | (s1, i) :-> (s2, o, L) <- m, s2 /= H ]
+  , runners   = go [ (s, s') | (s, O) :-> (s', _, R) <- m, s' /= H ]
   }
   where
-    goLeftStuck g
+    go :: Eq a => [(a, a)] -> [a]
+    go g
       | g == g'   = is
-      | otherwise = goLeftStuck g'
+      | otherwise = go g'
       where
         is  = map fst g
         g'  = [ e | e@(_, o) <- g, elem o is ]
-
-    goNoPath g
-      | g == g'   = is
-      | otherwise = goNoPath g'
-      where
-        is = [ i | i :-> _ <- g ]
-        g' = [ e | e@(_ :-> (s, _, _)) <- g, elem (s, O) is && elem (s, I) is ]
 
 unknown :: Int -> Int -> [Machine]
 unknown fuel n = [ m | m <- enum n, Left GiveUp <- [runTo fuel m] ]
@@ -160,10 +170,8 @@ runUnknown fuel n i = m <$ vrun fuel m
 loopCheck :: LoopAnalysis -> Int -> Config -> Config -> Maybe Reason
 loopCheck loop _ _ (s, tape@(Tape _ [] _))
   | elem (s, look tape) (leftStuck loop) = Just StuckLeft
-loopCheck loop _ (s1, Tape _ _ []) (s2, Tape _ _ [])
-  | s1 == s2 = Just RunAway
-loopCheck loop _ _ (s, tape)
-  | elem (s, look tape) (noPathToHalt loop) = Just NoPath
+loopCheck loop _ _ (s, Tape _ _ [])
+  | elem s (runners loop) = Just RunAway
 loopCheck _ n _ (s, Tape w _ _)
   | n > 100, w * 6 > n = Just TooWide
 loopCheck _ _ _ _ = Nothing
@@ -209,11 +217,189 @@ traceRun m = go (A, tape0)
         Nothing    -> []
         Just conf' -> conf : go conf'
 
+-- Running multiple machines at once --------------------------------------
+
+-- Idea: Enumerate machines as you run them
+-- * That way we don't have to enumerate transitions that are never taken! And there are a lot of
+--   machines with unused transitions.
+-- * You could also set a min value for termination and not generate a halting transition until that
+--   many steps!
+-- * Memory is a concern!
+
 unvisited :: Int -> Machine -> Set (State, Symbol)
 unvisited fuel m = Set.difference allStates visited
   where
-    allStates = Set.fromList [ i | i :-> _ <- m ]
+    allStates = Set.fromList [ i | i :-> (s, _, _) <- m, s /= H ]
     visited = snd $ runTo' mempty (\ _ (s, tape) -> Set.insert (s, look tape)) fuel m
+
+data Generator g = Generator
+  { initG          :: g
+  , genTransitions :: g -> Int -> Config -> [((State, Symbol, Dir), g)]
+      -- Called when we need a new transition
+  }
+
+dumbGenerator :: Int -> Generator ()
+dumbGenerator n = Generator () $ \ _ _ _ -> [((s, o, d), ()) | s <- states, o <- [O, I], d <- [L, R]]
+  where
+    states = take n [A ..] ++ [H]
+
+data GenSt = GenSt
+  { oldStates       :: [(State, Int)] -- number of open transitions in each old state
+  , newStates       :: [State]
+  , openTransitions :: Int
+  , rGraph          :: ReachGraph State
+  }
+  deriving (Show)
+
+-- BB(3)
+-- First implementation
+--    Terminated     26668  (68.8%)
+--    GiveUp          4294  (11.1%)
+--    StuckLeft       2176  ( 5.6%)
+--    RunAway         1029  ( 2.7%)
+--    Loop            4581  (11.8%)
+--    Total          38748
+-- Halt reachable
+--    Terminated     26668  (74.3%)
+--    GiveUp          3312  ( 9.2%)
+--    StuckLeft       1519  ( 4.2%)
+--    RunAway          629  ( 1.8%)
+--    Loop            3768  (10.5%)
+--    Total          35896
+-- Oops (only HOL)
+--    Terminated      6667  (41.9%)
+--    GiveUp           715  ( 4.5%)
+--    StuckLeft       1519  ( 9.6%)
+--    RunAway          629  ( 4.0%)
+--    Loop            3768  (23.7%)
+--    TooWide         2597  (16.3%)
+--    Total          15895
+-- Lower bound 45
+--    Terminated      5141  (35.8%)
+--    GiveUp           715  ( 5.0%)
+--    StuckLeft       1519  (10.6%)
+--    RunAway          629  ( 4.4%)
+--    Loop            3768  (26.2%)
+--    TooWide         2597  (18.1%)
+--    Total          14369
+-- Fixed and improved RunAway
+--    Terminated      5168  (35.9%)
+--    GiveUp           720  ( 5.0%)
+--    StuckLeft       1559  (10.8%)
+--    RunAway          710  ( 4.9%)
+--    Loop            3796  (26.3%)
+--    TooWide         2454  (17.0%)
+--    Total          14407
+
+smartGenerator :: Int -> Int -> Generator GenSt
+smartGenerator n lo = Generator{..}
+  where
+    initG = GenSt{..}
+      where oldStates       = [(A, 2)]
+            newStates       = take (n - 1) [B ..] ++ [H]  -- Halt as the last resort
+            openTransitions = 2
+            rGraph          = mempty
+    genTransitions g@GenSt{..} n (s, look -> i) = do
+      let new = take 1 newStates
+          states | openTransitions > 1 = new ++ map fst oldStates
+                 | otherwise           = new
+      s' <- states
+      let oldStates' = map dec oldStates
+            where dec (s', n) | s == s' = (s', n - 1)
+                  dec e                 = e
+          (old', new', open')
+            | [s'] == new = ((s', 2) : oldStates', drop 1 newStates, openTransitions + 1)
+            | otherwise   = (          oldStates',        newStates, openTransitions - 1)
+          rg' = addReachEdge s s' rGraph
+          exitStates = Set.fromList [ s | (s, n) <- old', n > 0 ]
+      -- Don't halt before the lower bound unless we have to
+      guard $ or [ n >= lo
+                 , s' /= H
+                 , openTransitions == 1
+                 ]
+      -- Make sure every closed state can reach an exit state
+      guard $ and [ not $ Set.disjoint rs exitStates
+                  | (s, 0) <- old'
+                  , let rs = fromMaybe mempty $ Map.lookup s rg'
+                  ]
+      o <- if s' == H then [O] else [O, I]
+      d <- if s' == H then [L] else [L, R]
+      pure ((s', o, d), g{ oldStates       = old'
+                         , newStates       = new'
+                         , openTransitions = open'
+                         , rGraph          = rg'
+                         })
+
+-- debug = go (initG g)
+--   where
+--     g = smartGenerator 3
+--     step gs (s, i) sod = head [ gs' | (sod', gs') <- genTransitions g gs 0 (s, Tape 0 [] (i : [])), sod' == sod ]
+--     go gs [] = gs
+--     go gs ((i :-> o) : ts) = go (step gs i o) ts
+
+data LoopDetector l s = LoopDetector
+  { emptyL   :: l                -- For an empty machine
+  , stepL    :: l -> Rule -> l   -- When adding a new rule
+  , initLs   :: s
+  , stepLoop :: l -> s -> Int -> Config -> Config -> Either Reason s
+  }
+
+noLoopDetector :: LoopDetector () ()
+noLoopDetector = LoopDetector () (\ _ _ -> ()) () (\ _ _ _ _ _ -> Right ())
+
+loopDetector :: LoopDetector (Machine, LoopAnalysis) (Set Config)
+loopDetector = LoopDetector{..}
+  where
+    emptyL            = ([], LoopAnalysis mempty mempty)
+    stepL (m, _) rule = (rule : m, loopAnalysis (rule : m))
+    initLs            = mempty
+    stepLoop (_, loop) seen n conf@(_, tape) conf'
+      | shouldCache, Set.member conf seen          = Left Loop
+      | Just reason <- loopCheck loop n conf conf' = Left reason
+      | otherwise                                  = Right seen'
+      where
+        shouldCache = n < 25 && sz <= 5
+        sz = tapeSize tape
+        seen' | shouldCache = Set.insert conf seen
+              | otherwise   = seen
+
+-- Parameterised over
+--  * generator for new transitions
+--  * loop detection
+-- Problem: can't start in the middle, so not easy to parallelize
+runExplore' :: Generator g
+            -> LoopDetector l ls
+            -> Int
+            -> Machine
+            -> [(Either Reason Int, Machine)]
+runExplore' Generator{..} LoopDetector{..} fuel m =
+    go fuel 0 initG initL initLs m initConf
+  where
+    initL = foldl stepL emptyL m
+
+    go 0 _ _ _ _ m _ = pure (Left GiveUp, reverse m)
+    go _ n _ _ _ m (H, _) = pure (Right n, reverse m)
+    go fuel !n g l ls m conf@(s, tape) = do
+      let step = case [ sod | si :-> sod <- m, si == (s, i) ] of
+            []  -> do
+              -- Add new transition
+              (sod, g') <- genTransitions g n conf
+              let rule = (s, i) :-> sod
+              pure (rule : m, g', stepL l rule, sod)
+            sod : _ -> pure (m, g, l, sod)
+      (m', g', l', (s', o, d)) <- step
+      let conf' = (s', move d $ write o tape)
+      case stepLoop l' ls n conf conf' of
+        Left r    -> pure (Left r, reverse m')
+        Right ls' -> go (fuel - 1) (n + 1) g' l' ls' m' conf'
+      where
+        i = look tape
+
+naiveExplore :: Int -> Int -> [(Either Reason Int, Machine)]
+naiveExplore n fuel = runExplore' (dumbGenerator n) noLoopDetector fuel []
+
+explore :: Int -> Int -> Int -> [(Either Reason Int, Machine)]
+explore n lo fuel = runExplore' (smartGenerator n lo) loopDetector fuel []
 
 -- Compiled machines ------------------------------------------------------
 
@@ -367,28 +553,46 @@ bruteForce bound ms = go 1 0 ms mempty
               Right{} -> Terminated
               Left r  -> NonTerminated r
         stats' = Map.insertWith (+) s 1 stats
-      -- case runTo bound m of
 
-      -- | n > best  = (n, m) : go' (i + 1) n tms
-      -- | otherwise = go' (i + 1) best tms
     go' i best tms stats
       | mod i 1000000 == 0 = trace (show i) $ go i best tms stats
       | otherwise          = go i best tms stats
 
+bruteExplore :: [(Either Reason Int, Machine)] -> ([(Int, Int, Machine)], Stats)
+bruteExplore xs = go 0 (zip [1..] xs) mempty
+  where
+    addStat k = Map.insertWith (+) k 1
+    go best [] stats = ([], stats)
+    go best ((i, (Left r, _)) : ms) !stats = go' best ms (addStat (NonTerminated r) stats)
+    go best ((i, (Right n, m)) : ms) !stats
+      | n > best = first ((n, i, m):) $ go' n ms stats'
+      | otherwise = go' best ms stats'
+      where
+        stats' = addStat Terminated stats
+
+    go' best ms@((i, _) : _) stats
+      | mod i 1000000 == 0 = trace (show i) $ go best ms stats
+    go' best ms stats = go best ms stats
+
+bruteIO :: ([(Int, Int, Machine)], Stats) -> IO (Int, Int, Machine)
+bruteIO (ms, stats) = do
+  mapM_ print ms
+  let why Terminated = "Terminated"
+      why (NonTerminated r) = show r
+      total = sum stats
+  sequence_ [ printf "%-10s %9d  (%4.1f%%)\n" (why r) n (100 * fromIntegral @_ @Double n / fromIntegral total)
+            | (r, n) <- Map.toList stats ]
+  printf "%-10s %9d\n" "Total" total
+  pure (last ms)
+
 main :: IO ()
 main = do
-  let brute bound ms0 = do
-        let (ms, stats) = bruteForce bound ms0
-        mapM_ print ms
-        let why Terminated = "Terminated"
-            why (NonTerminated r) = show r
-            total = sum stats
-        sequence_ [ printf "%-10s %9d  (%4.1f%%)\n" (why r) n (100 * fromIntegral @_ @Double n / fromIntegral total)
-                  | (r, n) <- Map.toList stats ]
-        printf "%-10s %7d\n" "Total" total
+  let brute bound ms0 = () <$ bruteIO (bruteForce bound ms0)
   map read <$> getArgs >>= \ case
     [n, bound, a, b] -> brute bound $ take b $ drop a $ enum n
-    [n, bound, a]    -> brute bound $ drop a $ enum n
+    -- [n, bound, a]    -> brute bound $ drop a $ enum n
+    [n, lo, bound]    ->
+      () <$ bruteIO (bruteExplore $ explore n lo bound)
     [n, bound]       -> brute bound $ enum n
     [n] ->
       print $ length $ enum n
@@ -404,25 +608,59 @@ main = do
     --   go 1 maxBound $ traceRun bb5'
 
 -- bb3
+-- Terminated    123,609  (47.0%)
+-- GiveUp            784  ( 0.3%)
+-- StuckLeft      37,423  (14.2%)
+-- RunAway        46,555  (17.7%)
+-- Loop           22,124  ( 8.4%)
+-- TooWide        32,385  (12.3%)
+-- Total         262,880
 
--- bb4
--- (Terminated,              49_529_149)
--- (NonTerminated GiveUp,       647_565)
--- (NonTerminated StuckLeft, 16_164_579)
--- (NonTerminated RunAway,   17_710_699)
--- (NonTerminated NoPath,     8_616_376)
--- (NonTerminated Loop,      11_754_926)
--- (NonTerminated TooWide,   15_900_802)
--- (Total,                  120_324_096)
+-- BB(4) (fuel: 10,000)
+-- (Terminated,              49,529,149)
+-- (NonTerminated GiveUp,       647,565)
+-- (NonTerminated StuckLeft, 16,164,579)
+-- (NonTerminated RunAway,   17,710,699)
+-- (NonTerminated NoPath,     8,616,376)
+-- (NonTerminated Loop,      11,754,926)
+-- (NonTerminated TooWide,   15,900,802)
+-- (Total,                  120,324,096)
 
--- Skeletons
--- Terminated  46_039_804  (44.0%)
--- GiveUp         613_403  ( 0.6%)
--- StuckLeft   14_430_267  (13.8%)
--- RunAway     16_801_803  (16.1%)
--- Loop        11_365_166  (10.9%)
--- TooWide     15_405_685  (14.7%)
--- Total      104_656_128
+-- Skeletons (13m30s)
+--    Terminated  46,039,804  (44.0%)
+--    GiveUp         613,403  ( 0.6%)
+--    StuckLeft   14,430,267  (13.8%)
+--    RunAway     16,801,803  (16.1%)
+--    Loop        11,365,166  (10.9%)
+--    TooWide     15,405,685  (14.7%)
+--    Total      104,656,128
+
+-- Explore (2m41s)
+--    Terminated    932,925  (37.6%)
+--    GiveUp        237,981  ( 9.6%)
+--    StuckLeft     166,432  ( 6.7%)
+--    RunAway        73,188  ( 2.9%)
+--    Loop          675,060  (27.2%)
+--    TooWide       395,934  (16.0%)
+--    Total       2,481,520
+
+-- Lower bound 3500 (2m40s)
+--    Terminated    756,284  (32.8%)
+--    GiveUp        237,981  (10.3%)
+--    StuckLeft     166,432  ( 7.2%)
+--    RunAway        73,188  ( 3.2%)
+--    Loop          675,060  (29.3%)
+--    TooWide       395,934  (17.2%)
+--    Total       2,304,879
+
+-- Fixed RunAway (2m38s)
+--    Terminated    761,834  (32.9%)
+--    GiveUp        239,243  (10.3%)
+--    StuckLeft     173,502  ( 7.5%)
+--    RunAway        80,837  ( 3.5%)
+--    Loop          682,361  (29.4%)
+--    TooWide       380,319  (16.4%)
+--    Total       2,318,096
 
 -- Examples ---------------------------------------------------------------
 
@@ -558,18 +796,31 @@ sim_bb5 extras = go 3
 --       , (E,O) :-> (B,O,R)
 --       , (E,I) :-> (C,I,R) ]
 
--- Score: 97461, index: 3_826_129_157
+-- -- Score: 97461, index: 3_826_129_157
+-- bb5 :: Machine
+-- bb5 = [ (A,O) :-> (B,O,L)
+--       , (A,I) :-> (C,O,R)
+--       , (B,O) :-> (D,O,L)
+--       , (B,I) :-> (H,O,L)
+--       , (C,O) :-> (E,I,L)
+--       , (C,I) :-> (C,I,R)
+--       , (D,O) :-> (C,I,L)
+--       , (D,I) :-> (A,I,L)
+--       , (E,O) :-> (A,O,R)
+--       , (E,I) :-> (A,O,L) ]
+
+-- -- Score: 115854, index: 4_082_614_040
 bb5 :: Machine
 bb5 = [ (A,O) :-> (B,O,L)
       , (A,I) :-> (C,O,R)
       , (B,O) :-> (D,O,L)
-      , (B,I) :-> (H,O,L)
-      , (C,O) :-> (E,I,L)
-      , (C,I) :-> (C,I,R)
-      , (D,O) :-> (C,I,L)
-      , (D,I) :-> (A,I,L)
-      , (E,O) :-> (A,O,R)
-      , (E,I) :-> (A,O,L) ]
+      , (B,I) :-> (A,I,L)
+      , (C,O) :-> (A,I,R)
+      , (C,I) :-> (E,O,R)
+      , (D,O) :-> (E,I,R)
+      , (D,I) :-> (H,O,L)
+      , (E,O) :-> (C,O,R)
+      , (E,I) :-> (A,I,R) ]
 
 -- Score: 71076
 bb5Andreas :: Machine
@@ -594,3 +845,27 @@ example = [ (A, O) :-> (B, I, R)
           , (C, I) :-> (A, I, R)
           ]
 
+-- QuickCheck tests -------------------------------------------------------
+
+instance Arbitrary State where
+  arbitrary = elements [A .. F]
+
+genGraph :: Gen (ReachGraph State)
+genGraph = foldr (uncurry addReachEdge) mempty <$> (listOf ((,) <$> elements [A .. F] <*> elements [A .. F]))
+
+prop_reach_idem :: [(State, State)] -> State -> State -> Property
+prop_reach_idem es s1 s2 = add g === add (add g)
+  where
+    g = foldr (uncurry addReachEdge) mempty es
+    add = addReachEdge s1 s2
+
+prop_reach :: [(State, State)] -> Property
+prop_reach es = whenFail (print g) $ bad === []
+  where
+    g = foldr (uncurry addReachEdge) mempty es
+    bad = [ (s, s')
+          | (s, ss) <- Map.toList g
+          , s' <- Set.toList ss
+          , let ss' = fromMaybe mempty $ Map.lookup s' g
+          , not $ Set.isSubsetOf ss' ss
+          ]
